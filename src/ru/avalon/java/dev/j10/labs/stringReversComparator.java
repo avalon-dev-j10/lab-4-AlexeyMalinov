cf3f869998331bf9a931b3872f07fe08cca6aca8
AlexeyMalinov <alexeymalinov@yandex.ru>
2019-03-30 21:59:54 +0300

    Скорректирован класс stringReversComparator

--- src/ru/avalon/java/dev/j10/labs/stringReversComparator.java
+++ src/ru/avalon/java/dev/j10/labs/stringReversComparator.java
@@ -9,16 +9,15 @@ public class stringReversComparator implements Comparator {
 
     /**
      * Выполняет сравнение строк
-     * @param o1
-     * @param o2
+     *
+     * @param object1
+     * @param object2
      * @return
      */
     @Override
-    public int compare(Object o1, Object o2) {
-        if (o1 instanceof String && o2 instanceof String){
-            String string1 = (String) o1;
-            String string2 = (String) o2;
-            return string2.compareTo(string1);
+    public int compare(Object object1, Object object2) {
+        if (object1 instanceof String && object2 instanceof String) {
+            return ((String) object2).compareTo((String) object1);
         }
         return -1;
     }
